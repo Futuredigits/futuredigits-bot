8e73a9b33daf18117f986df693a56bb015bc1063
FutureDigits <futuredigitstg@gmail.com>
2025-07-15 16:55:09 +0300

    _init_.py update

---
+++ handlers/__init__.py
@@ -0,0 +1,7 @@
+from .life_path import *
+from .soul_urge import *
+from .expression import *
+from .personality import *
+from .destiny import *
+from .birthday_number import *
+from .compatibility import *
